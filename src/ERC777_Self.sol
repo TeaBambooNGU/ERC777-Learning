3557cd6e3bdd59a1a96332ad898f87c794ddd677
tea bamboo <teabamboongu@gmail.com>
2023-12-01 00:26:42 +0800

    ERC777_self code完成

--- src/ERC777_Self.sol
+++ src/ERC777_Self.sol
@@ -27,7 +27,7 @@ abstract contract ERC777 is Context, IERC777, IERC20 {
     bytes32 private constant _TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     // 默认操作者列表 不用于读取
     address[] private _defaultOperatorsArray;
-    // 用于索引默认操作者状态 不可变 可以被撤销(tracked in __revokedDefaultOperators).
+    // 用于索引默认操作者状态 不可变 可以被标记撤销(tracked in __revokedDefaultOperators).
     mapping (address => bool) private _defaultOperators;
 
     // For each account, a mapping of its operators and revoked default operators.
@@ -145,8 +145,9 @@ abstract contract ERC777 is Context, IERC777, IERC20 {
             _balances[from] = fromBalance - amount;
         }
         _balances[to] += amount;
-
+        // ERC777的 Sent事件
         emit Sent(operator, from, to, amount, userData, operatorData);
+        // ERC20的 Transfer事件
         emit Transfer(from, to, amount);
     }
 
@@ -209,45 +210,209 @@ abstract contract ERC777 is Context, IERC777, IERC20 {
 
         _callTokensReceived(operator, from, to, amount, userData, operatorData, requireReceptionAck);
     }
-
+    // ERC777 定义的转账函数， 同时触发 ERC20的 `Transfer` 事件
     function send(
         address recipient,
         uint256 amount,
         bytes calldata data
-    ) public virtual override {}
+    ) public virtual override {
+        _send(_msgSender(),recipient,amount,data,"",true);
+    }
 
-    function burn(uint256 amount, bytes calldata data) external override {}
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Unlike `send`, `recipient` is _not_ required to implement the {IERC777Recipient}
+     * interface if it is a contract.
+     *
+     * Also emits a {Sent} event.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(recipient != address(0), "ERC777: transfer to the zero address");
+        address from = _msgSender();
+
+        _callTokensToSend(from, from, recipient, amount, "", "");
+
+        _move(from, from, recipient, amount, "", "");
+
+        _callTokensReceived(from, from, recipient, amount, "", "", false);
+
+        return true;
+    }
+
+    /**
+     * @dev Burn tokens
+     * @param from address token holder address
+     * @param amount uint256 amount of tokens to burn
+     * @param userData bytes extra information provided by the token holder
+     * @param operatorData bytes extra information provided by the operator (if any)
+     */
+    function _burn(
+        address from,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) internal virtual {
+        require(from != address(0), "ERC777: burn from zero address");
+        address operator = _msgSender();
+        
+        _callTokensToSend(operator, from , address(0), amount, userData, operatorData);
+
+        _beforeTokenTransfer(operator, from, address(0), amount);
+
+        uint256 fromBalance = _balances[from];
+
+        require(fromBalance > amount, "ERC777: burn amount exceeds balance");
+
+        unchecked {
+            _balances[from] -= amount;
+        }
+
+        _totalSupply -= amount;
+
+        emit Burned(operator, from, amount, userData, operatorData);
+        emit Transfer(from, address(0), amount);
+    }
+
+    /**
+     * 销毁函数 实际使用时 需要使用 _burn(msg.sender,amount,data, "");
+     * @param amount 金额
+     * @param data 代币拥有者的数据
+     * Emits {Burn} {ERC20-Transfer}
+     */
+    function burn(uint256 amount, bytes calldata data) public virtual override {
+        _burn(_msgSender(),amount,data, "");
+    }
 
     function isOperatorFor(
         address operator,
         address tokenHolder
-    ) external view override returns (bool) {}
+    ) public view virtual override returns (bool) {
+        return 
+            operator == tokenHolder ||
+            (_defaultOperators[operator] && !_revokedDefaultOperators[tokenHolder][operator]) ||
+            _operators[tokenHolder][operator];
+    }
 
-    function authorizeOperator(address operator) external override {}
+    // 授权操作者 实际使用时需要校验权限
+    function authorizeOperator(address operator) public virtual override {
+        require(_msgSender() != operator, "ERC777: anthorizing self as operator");
 
-    function revokeOperator(address operator) external override {}
+        if (_defaultOperators[operator]) {
+            delete _revokedDefaultOperators[_msgSender()][operator];
+        }else {
+            _operators[_msgSender()][operator] = true;
+        }
 
-    function defaultOperators()
-        external
-        view
-        override
-        returns (address[] memory)
-    {}
+        emit AuthorizedOperator(operator, _msgSender());
+    }
+    // 撤销操作者 实际使用时需要校验权限
+    function revokeOperator(address operator) external override {
+        require(operator != _msgSender(), "ERC777: revoking self as operator");
 
+        if(_defaultOperators[operator]){
+            _revokedDefaultOperators[_msgSender()][operator] == true;
+        }else {
+            delete _operators[_msgSender()][operator];
+        }
+
+        emit RevokedOperator(operator, _msgSender());
+    }
+
+    function defaultOperators() public view virtual override returns (address[] memory){
+            return _defaultOperatorsArray;
+    }
+
+    /**
+     * @dev See {IERC777-operatorSend}.
+     *
+     * Emits {Sent} and {IERC20-Transfer} events.
+     */
     function operatorSend(
-        address sender,
+        address account,
         address recipient,
         uint256 amount,
         bytes calldata data,
         bytes calldata operatorData
-    ) external override {}
+    ) public virtual override {
+        require(isOperatorFor(_msgSender(), account),"ERC777: caller is not an operator for holder");
+       _send(account, recipient, amount, data, operatorData, true);
+    }
 
+    /**
+     * @dev See {IERC777-operatorBurn}.
+     *
+     * Emits {Burned} and {IERC20-Transfer} events.
+     */
     function operatorBurn(
         address account,
         uint256 amount,
         bytes calldata data,
         bytes calldata operatorData
-    ) external override {}
+    )  public virtual override  {
+        require(isOperatorFor(_msgSender(), account),"ERC777: caller is not an operator for holder");
+        _burn(account,amount,data,operatorData);
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     *
+     * Note that operator and allowance concepts are orthogonal: operators may
+     * not have allowance, and accounts with allowance may not be operators
+     * themselves.
+     */
+    function allowance(address holder, address spender) public view virtual override returns(uint256)  {
+        return _allowances[holder][spender];
+    }
+
+    function _approve(address holder, address spender, uint256 value) internal {
+        require(holder != address(0), "ERC777: approve from the zero address");
+        require(spender != address(0), "ERC777: approve to the zero address");
+
+        _allowances[holder][spender] = value;
+        emit Approval(holder, spender, value);
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Note that accounts cannot have allowance issued by their operators.
+     */
+    function approve(address spender, uint256 value) public virtual override returns (bool) {
+        address holder = _msgSender();
+        _approve(holder, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Note that operator and allowance concepts are orthogonal: operators cannot
+     * call `transferFrom` (unless they have allowance), and accounts with
+     * allowance cannot call `operatorSend` (unless they are operators).
+     *
+     * Emits {Sent}, {IERC20-Transfer} and {IERC20-Approval} events.
+     */
+    function transferFrom(
+        address holder,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        require(recipient != address(0), "ERC777: transfer to the zero address");
+        require(holder != address(0), "ERC777: transfer from the zero address");
+
+        address spender = _msgSender();
+
+        _callTokensToSend(spender,holder,recipient, amount, "", "");
+
+        _move(spender, holder, recipient, amount, "", "");
+
+        _callTokensReceived(spender, holder, recipient, amount, "", "", false);
+
+        return true; 
+    }
+
+
 }
         
     
