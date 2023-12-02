e292da267e47204c8c812776ed16540985730f94
tea bamboo <teabamboongu@gmail.com>
2023-12-02 23:45:53 +0800

    ERC777 code

--- src/Tang777.sol
+++ src/Tang777.sol
@@ -2,23 +2,27 @@
 pragma solidity ^0.8.20;
 
 import "./ERC777_Self.sol";
-
+import "openzeppelin-contracts/contracts/interfaces/IERC1820Implementer.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 event RecveiveTokens(address indexed from, uint256 indexed amount);
 
 event SendTokens(address indexed to, uint256 indexed amount);
 
-contract Tang777 is ERC777 {
+contract Tang777 is ERC777,Ownable {
 
     bytes32 private constant _TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
     bytes32 private constant _TOKENS_SENDER_INTERFACE_HASH = keccak256("ERC777TokensSender");
+    bytes32 private constant _ERC1820_ACCEPT_MAGIC = keccak256(abi.encodePacked("ERC1820_ACCEPT_MAGIC"));
+    // 委托该合约实现接口的账号
+    mapping (address => mapping (bytes32 => bool)) private  interfaceForAccounts;
 
 
     constructor (
         string memory name_, 
         string memory symbol_, 
         address[] memory defaultOperators
-    )ERC777(name_, symbol_, defaultOperators){
+    )ERC777(name_, symbol_, defaultOperators) Ownable(msg.sender){
         uint initialSupply = 710 * 10 ** 18;
         _mint(msg.sender,initialSupply,"","");
 
@@ -51,6 +55,23 @@ contract Tang777 is ERC777 {
         bytes calldata operatorData
     ) external {
         emit SendTokens(to, amount);
+    }
+    /// @notice 指示合约是否为地址 “addr” 实现接口 “interfaceHash”。
+    /// 用于 一个普通的用户地址(账号)，委托一个合约来监听代币的转出
+    /// @param interfaceHash 接口名称的 keccak256 哈希值
+    /// @param account 为哪一个账号实现接口
+    /// @return 有当合约为账号'account'实现'interfaceHash'时返回 ERC1820_ACCEPT_MAGIC
+    function canImplementInterfaceForAddress(bytes32 interfaceHash, address account) external view returns (bytes32) {
+        if (interfaceForAccounts[account][interfaceHash]) {
+            return _ERC1820_ACCEPT_MAGIC;
+        }else {
+            return bytes32(0x00);
+        }
+        
+    }
+
+    function setInterfaceForAccounts(address account, bytes32 interfaceHash) external onlyOwner {
+        interfaceForAccounts[account][interfaceHash] = true;
     }
 
     
